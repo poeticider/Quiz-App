35a64a94ed480c534f858f71b8b9f78b4fe9f864
Orange Workbook <d.riley0189@gmail.com>
2023-11-09 12:15:08 +0000

    adds function to cycle through questions on click

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,5 @@
+//-----------------------------------------DATA OBJECTS------------------------------------------------
+
 //questions added as string values to obj
 const questionsListObj = {
     q1:"Which of the following is NOT a sub-genre of fighting games?",
@@ -24,11 +26,12 @@ const answersListObj = {
 }
 
 
-
+//------------------------------------------------LOGIC-----------------------------------------------
 
 
 
 const quiz = () => {
+
     let startBtn = document.getElementById("start");
     startBtn.classList.add("hide");
 
@@ -38,14 +41,11 @@ const quiz = () => {
     let questionTitle = document.getElementById("questionTitle");
     let choices = document.getElementById("choices");
 
-    
-
-
+    //count used to cycle through nested object questions
     let count = 0;
     
+    
 
-    let answerKeys = Object.keys(answersListObj);
-    //console.log(answerKeys[count])
     let qCount = "q" + (count +1)
     //console.log(qCount)
     let qTitleContent = questionsListObj[qCount];
@@ -53,39 +53,58 @@ const quiz = () => {
     //console.log(choicesContent)
 
     let questionsLength = Object.keys(choicesContent).length;
-    //console.log(Object.keys(choicesContent).length);
-
-
-
-
-
-    if (qCount == answerKeys[count]) {
+    console.log(Object.keys(choicesContent).length);
+    
+    //function to manipulate the dom to create a new set of question buttons. 
+    //The nested objected is chosen based on the key (ie q1)
+    const nextQuestion = () => {
         questionTitle.textContent = qTitleContent;
+        //removes previous set of buttons
+        choices.innerHTML = ""; 
 
-        for (let i=0; i < questionsLength; i++) {
-            //var stores question keys
-            let keys = Object.keys(choicesContent);
-            //console.log(keys[i]);
+        //stores keys for choicesContent as an array
+        let keys = Object.keys(choicesContent);
+        //console.log(keys)
 
+        for (let i = 0; i < questionsLength; i++) {
             let newBtn = document.createElement("button");
             newBtn.textContent = keys[i];
             choices.appendChild(newBtn);
-        
+        }
+    }
+
+    //initial function call for first set of questions
+    nextQuestion();
+
+
+    addEventListener('click', function(e) {
+        //event listener target #choices children buttons only
+        if (e.target.tagName === 'BUTTON' && e.target.parentElement.id === 'choices') {
+            //when a button is clicked, the count increments
+            count++
+
+            //converts object into array of keys. this is done because .length will return undefined on an object
+            let answerKeys = Object.keys(answersListObj);
+
+            //permitted the count is less than the amount of questions...
+            if (count < answerKeys.length) {
+                //...content values is updated using qCount...
+                qCount = "q" + (count + 1);
+                qTitleContent = questionsListObj[qCount];
+                choicesContent = answersListObj[qCount];
+                questionsLength = Object.keys(choicesContent).length;
+
+                //...and nextQuestion is called to update the html
+                nextQuestion();
+
+            } else {
+                //once count reaches answerKeys length, quiz will end
+                console.log("Quiz completed logic goes here");
+            }
+
         }
 
+    });
 
-        addEventListener('click', function(e) {
-            //event listener target #choices children buttons only
-            if (e.target.tagName === 'BUTTON' && e.target.parentElement.id === 'choices') {
-                count++
-                console.log(count);
-                
-            }
-        });
-
-    
-    
-
-    }
 }
 
